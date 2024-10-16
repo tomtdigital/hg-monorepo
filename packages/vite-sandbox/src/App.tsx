12adc18cce438a15216212c25aee4b87ff4c9f46
Tom Davies <tom.tdigital@gmail.com>
2024-10-16 13:50:44 +0100

    material ui install

--- packages/vite-sandbox/src/App.tsx
+++ packages/vite-sandbox/src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import AboutPage from "./pages/about";
 import HomePage from "./pages/index";
 
