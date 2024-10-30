71beeac95f656796c2e77b8883d9fa2cae1cb17f
Tom Davies <tom.tdigital@gmail.com>
2024-10-30 14:03:49 +0000

    dependent form fields

--- packages/hg-admin/app/components/test-form.tsx
+++ packages/hg-admin/app/components/test-form.tsx
@@ -1,37 +1,66 @@
 "use client";
 
-import { StyledTextField, StyledSelectField } from "@hannagrams/ui";
-import { Button, MenuItem, Select } from "@mui/material";
-import { useForm, Controller } from "react-hook-form";
+import { StyledSelectField, StyledTextField } from "@hannagrams/ui";
+import { Button, MenuItem } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
 
 type TestFormValues = {
   name: string;
-  gender: string;
+  employmentStatus: string;
 };
 
 const defaultValues = {
-  name: "Dave",
-  gender: "m",
+  name: "",
+  employmentStatus: "se",
 };
 
 export default function TestForm() {
-  const { handleSubmit, reset, watch, control, register } = useForm({
+  const {
+    handleSubmit,
+    reset,
+    watch,
+    control,
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm({
     defaultValues,
+    mode: "all",
   });
   const onSubmit = (data: TestFormValues) => console.log(data);
+  const name = watch("name");
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <StyledTextField {...register("name")} />
+      <StyledTextField
+        {...register("name", {
+          minLength: { value: 4, message: "Sort your act out" },
+        })}
+        onBlur={() => {
+          trigger("employmentStatus");
+        }}
+      />
+      {errors?.name?.message && <p>{errors.name.message}</p>}
       <Controller
         render={({ field }) => (
           <StyledSelectField {...field}>
-            <MenuItem value="m">Male</MenuItem>
-            <MenuItem value="f">Female</MenuItem>
+            <MenuItem value="e">Employed</MenuItem>
+            <MenuItem value="u">Unemployed</MenuItem>
+            <MenuItem value="se">Self Employed</MenuItem>
           </StyledSelectField>
         )}
         control={control}
-        name="gender"
+        name="employmentStatus"
+        rules={{
+          validate: (value: string) => {
+            if (name === "Elon Musk" && value !== "se")
+              return "Elon Musk is self employed!";
+            return true;
+          },
+        }}
       />
+      {errors?.employmentStatus?.message && (
+        <p>{errors.employmentStatus.message}</p>
+      )}
       <Button
         type="button"
         sx={{ padding: "1em", backgroundColor: "purple" }}
@@ -39,6 +68,7 @@ export default function TestForm() {
       >
         Reset
       </Button>
+      {/* NB: Don't use the SX prop due to performance impact */}
       <Button sx={{ padding: "1em", backgroundColor: "yellow" }} type="submit">
         Submit
       </Button>
